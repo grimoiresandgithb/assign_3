b97455db2cbde17b3fcc0e95f8a740aaf845b13d
Alyssa DaCosta <alyssalrd@gmail.com>
2026-03-25 13:48:11 -0600

    SLL methods added and functional

--- src/sait/sll/utility/Node.java
+++ src/sait/sll/utility/Node.java
@@ -1,47 +1,32 @@
 package sait.sll.utility;
 
-
 /**
  * Represents single node in a singly linked list
  * Stores generic Object data and a reference to the next node
  */
 public class Node {
 
-	private Object data;
-	private Node next;
-	
-	/**
-	 * Creates a node containing the given data
-	 * @param data The data stored in this node
-	 */
-	
-	public Node (Object data) {
-		this.data = data;
-		this.next = null;
-	}
-	
-	/**
-	 * Gets the data stored in the node
-	 * @return The data object
-	 */
-	public Object getData() {
-		return data;
-	}
-	
-	/**
-	 * Gets the next node in the list
-	 * @return The next node
-	 */
-	public Node getNext() {
-		return next;
-	}
-	
-	/**
-	 * Sets the next node reference
-	 * @param next The next node
-	 */
-	public void setNext(Node next) {
-		this.next = null;
-	}
-	
+    private Object data;
+    private Node next;
+
+    public Node(Object data) {
+        this.data = data;
+        this.next = null;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;  
+    }
 }
